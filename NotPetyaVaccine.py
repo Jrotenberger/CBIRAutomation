6caf11bdecb59104d0d952e3e79437b9a9fb2227
jjfallete <jfagel@allete.com>
2017-06-30 15:33:06 -0500

    Update NotPetyaVaccine.py

--- NotPetyaVaccine.py
+++ NotPetyaVaccine.py
@@ -30,27 +30,34 @@ while len(SensorsAwaitingVaccine) != 0:  # We're going to loop over these indefi
     for s in SensorsAwaitingVaccine:
         if 'online' in s.status.lower():
 
-            print("[INFO] Establishing CBLR session to " + str(s.hostname))
-            session = c.live_response.request_session(s.id)
-            print("[SUCCESS] Connected on CBLR Session #" + str(session.session_id))
-
-            try: session.create_directory("C:\Windows\CarbonBlack\Tools")
-            except Exception: pass  # Existed already
-
-            try: session.put_file(open(batch_path + r"\nopetyavac.bat", "rb"), r"C:\Windows\CarbonBlack\Tools\nopetyavac.bat")
-            except Exception: pass  # Existed already
-
             try:
-                session.create_process(r'C:\\Windows\\CarbonBlack\\Tools\\nopetyavac.bat', False)
-                SensorsAwaitingVaccine.remove(s)
-                print ("[SUCCESS] " + str(s.hostname) + " vaccinated against NotPetya Ransomware!")
+                
+                print("[INFO] Establishing CBLR session to " + str(s.hostname))
+                session = c.live_response.request_session(s.id)
+                print("[SUCCESS] Connected on CBLR Session #" + str(session.session_id))
 
-            except Exception, err:
+                try: session.create_directory("C:\Windows\CarbonBlack\Tools")
+                except Exception: pass  # Existed already
+
+                try: session.put_file(open(batch_path + r"\nopetyavac.bat", "rb"), r"C:\Windows\CarbonBlack\Tools\nopetyavac.bat")
+                except Exception: pass  # Existed already
+
+                try:
+                    session.create_process(r'C:\\Windows\\CarbonBlack\\Tools\\nopetyavac.bat', False)
+                    SensorsAwaitingVaccine.remove(s)
+                    print ("[SUCCESS] " + str(s.hostname) + " vaccinated against NotPetya Ransomware!")
+
+                except Exception, err:
+                    print("[ERROR]: " + str(err))
+                    print ("[FAILURE] " + str(s.hostname) + " was NOT vaccinated against NotPetya Ransomware! Will try again later.")
+
+                session.close()  # We could delete nopetyavac.bat from the remote sensor CB folder prior, but not required.
+                print("[INFO] CBLR session to " + str(s.hostname) + " has been closed")
+                
+            except Exception, err:  # Added to catch other possible errors, like session timeout during connection attempt.
                 print("[ERROR]: " + str(err))
                 print ("[FAILURE] " + str(s.hostname) + " was NOT vaccinated against NotPetya Ransomware! Will try again later.")
-
-            session.close()  # We could delete nopetyavac.bat from the remote sensor CB folder prior, but not required.
-            print("[INFO] CBLR session to " + str(s.hostname) + " has been closed")
+            
             print("[INFO] There are now " + str(len(SensorsAwaitingVaccine)) + " sensors awaiting the vaccination...")
 
 print("[INFO] Script completed.")
