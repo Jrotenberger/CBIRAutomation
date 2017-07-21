3d21987f2629dfda9b278c2c9032a0d78b13b6c1
jjfallete <jfagel@allete.com>
2017-07-21 11:31:43 -0500

    Update Memory Capture.py

--- Memory Capture.py
+++ Memory Capture.py
@@ -17,7 +17,7 @@ sensor = c.select(Sensor, sensor_id)
 
 with sensor.lr_session() as session:          # this will wait until the Live Response session is established
     session.put_file(open("c:\\temp\\winpmem.exe", "rb") ,"c:\\windows\\CarbonBlack\\winpmem.exe")
-    session.create_process("c:\\windows\\CarbonBlack\\winpmem.exe \\netapp-recc-1a\carbonblk_forensics$\Memory.raw")
+    session.create_process("c:\\windows\\CarbonBlack\\winpmem.exe \\win\path\Memory.raw")
     time.sleep(90)
     print "Memory Capture Started"
     open("c:\\temp\\{0}.txt".format(sensor.hostname), "wb").write(session.get_file("c:\\windows\\CarbonBlack\\jrt.txt"))
